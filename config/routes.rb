8d9e7d98a7b40e7159b7838916a91cd7f833c4f9
Francisco Bravo <franciscobravo3220@gmail.com>
2013-11-20 15:33:56 -0500

    Pruebas ya casi

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Picturesque::Application.routes.draw do
-  resources :loaderio_7abd2d23994e8fb4f6f945f20b5204dbs
-
-  resources :ones
-
-  resources :loaderio_1s
-
   root :to => "paintings#new"
 
   resources :galleries
